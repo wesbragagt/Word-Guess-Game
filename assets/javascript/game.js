ab60a439652a9af9cbb0ae91dda275d4e4ada559
Weslley Braga <wesbragagt@gmail.com>
2019-02-06 17:40:42 -0600

    managed to add basic functionalities to my game such as key pressed and number of tries and generate a randomword

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+
+// Variables interacting with the DOM
+//display key that was pressed
+var userText = document.getElementById("usertext");
+
+var numberGuess = document.getElementById("number-guess");
+
+// display the amount of times you have tried
+var letterPicked = document.getElementById("letter-guess");
+var letterGuess = [];
+
+//word box text
+var wordtext = document.getElementById("wordtext");
+var wordArray = ["word1","word2","word3","word4","word5","word6","word7"];
+
+
+// Keypress function
+document.onkeyup = function(event) {
+	userText.textContent = event.key;
+	letterPicked.textContent = letterGuess.push(event.key); // How can I make this add the key pressed letter?
+
+};
+
+// calling random word function on loading page to write the element which was picked by the random function
+document.onload = randomWord(wordArray);
+
+// created a random function, to go through wordArray and pick one element.
+function randomWord(wordArray) {
+	wordtext.textContent = wordArray[Math.floor(Math.random()*wordArray.length)];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Testing Console
+// console.log(wordtext);
+// randomWord(wordArray);
