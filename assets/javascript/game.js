4174e2c81cadd906cd44942151174cae525e3b1f
Weslley Braga <wesbragagt@gmail.com>
2019-02-08 09:24:32 -0600

    added style css to my game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,22 +11,25 @@ var letterGuess = [];
 
 //word box text
 var wordtext = document.getElementById("wordtext");
-var wordArray = ["word1","word2","word3","word4","word5","word6","word7"];
+var wordArray = ["word1", "word2", "word3", "word4", "word5", "word6", "word7"];
+var wordField = randomWord(wordArray);
 
-
-// Keypress function
-document.onkeyup = function(event) {
+// Document functions
+document.onkeyup = function (event) {
 	userText.textContent = event.key;
 	letterPicked.textContent = letterGuess.push(event.key); // How can I make this add the key pressed letter?
-
+	console.log(letterGuess)
 };
 
 // calling random word function on loading page to write the element which was picked by the random function
 document.onload = randomWord(wordArray);
 
+
+//Functions
+
 // created a random function, to go through wordArray and pick one element.
 function randomWord(wordArray) {
-	wordtext.textContent = wordArray[Math.floor(Math.random()*wordArray.length)];
+	wordtext.textContent = wordArray[Math.floor(Math.random() * wordArray.length)];
 }
 
 
